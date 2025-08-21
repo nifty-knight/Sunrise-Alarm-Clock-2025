dfc45d8d04ca23e6838ccb3fc78a0ceed40cb4a9
Breanne Chan <107957421+nifty-knight@users.noreply.github.com>
2025-08-20 20:03:43 -0700

    Added high-level variables and sample program

--- src/Full_program_v3.9/Full_program_v3.9.ino
+++ src/Full_program_v3.9/Full_program_v3.9.ino
@@ -1,15 +1,38 @@
+#include "AlarmSystem.cpp" // TODO: replace all with .h
+#include "DisplayController.cpp"
+#include "InputController.cpp"
+
+// Pin numbers for the RTC Module
+const uint8_t PIN_CLK = 7;
+const uint8_t PIN_DAT = 6;
+const uint8_t PIN_ENA = 5;  // RST on the module itself
+
+AlarmSystem alarmSystem;
+DisplayController displayController;
+InputController inputController;
+
+// Main file where program is run
+
 void setup() {
   // put your setup code here, to run once:
-
+  alarmSystem = AlarmSystem();
+  displayController = DisplayController();
+  inputController = InputController();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  inputController.update();
+  displayController.update(InputEvent::None); // note: don't know if this will mess with the program's operations; might not need this function, and only updates on inputController call
+  alarmSystem.update(InputEvent::None);
+  
 }
 
 /* Current coding TODOs:
 - Create .cpp files with stub functions and fields for all classes
+- Create test scenarios for all functions (don't implement a full testing framework YET)
+- implement some testing + test framework for more easily tested functions/classes - put it in a different folder than the this sketch
 - Add one-line class description above class declaration for all .cpp files
 - Add .h files for all .cpp files
 - Modify all includes to include .h, not .cpp
