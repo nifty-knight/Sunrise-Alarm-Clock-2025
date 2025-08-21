e8d856696d567e0717de7b5aef3b764b9ed622f3
Breanne Chan <107957421+nifty-knight@users.noreply.github.com>
2025-08-20 18:46:23 -0700

    Create InputController.cpp

---
+++ src/Full_program_v3.9/InputController.cpp
@@ -0,0 +1,9 @@
+// #include "Observer.cpp" // TODO: change these includes to be .h files
+#include "Subject.cpp"
+
+// Responsible for processing user input and informing registered clients/observers
+class InputController : Subject {
+  private:
+    // const int delayTime; // note: should not need this line of code- use edge detection instead
+    void notifyObservers(InputEvent i) override;
+};
