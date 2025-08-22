41ec4ff03ddbfa8af7a52c7027e68e13c754423c
Breanne Chan <107957421+nifty-knight@users.noreply.github.com>
2025-08-21 18:46:20 -0700

    Added AlarmSystem.h file

--- src/Full_program_v3.9/AlarmSystem.cpp
+++ src/Full_program_v3.9/AlarmSystem.cpp
@@ -1,28 +1,38 @@
-#ifndef ALARMSYSTEM_H
-#define ALARMSYSTEM_H
+// #ifndef ALARMSYSTEM_H
+// #define ALARMSYSTEM_H
 
-#include "Observer.h"
-#include <Ds1302.h>
+// #include "Observer.h"
+// #include <Ds1302.h>
 
-// Controls the functionality of the clock & alarm system 
-class AlarmSystem : public Observer {
-  private:
-    Ds1302::DateTime alarmTime;
-    short fadeTime = 20;
-    bool alarmIsOn = false;
-    unsigned long ledStartTime;
-    unsigned long waitTime;
+// // Controls the functionality of the clock & alarm system 
+// class AlarmSystem : public Observer {
+//   private:
+//     Ds1302::DateTime alarmTime;
+//     short fadeTime = 20;
+//     bool alarmIsOn = false;
+//     unsigned long ledStartTime;
+//     unsigned long waitTime;
 
-    void updateLight();
-    void daylightSavingsChange(short change); // Could make this bool since only two states- +1hr or -1hr
-    void setAlarmTime(Ds1302::DateTime t);
-    void setAlarmState(bool s);
+//     void updateLight();
+//     void daylightSavingsChange(short change); // Could make this bool since only two states- +1hr or -1hr
+//     void setAlarmTime(Ds1302::DateTime t);
+//     void setAlarmState(bool s);
 
-  public:
-    void update(InputEvent i) override;
-    Ds1302::DateTime getAlarmTime();
-    bool getAlarmState();
+//   public:
+//     void update(InputEvent i) override;
+//     Ds1302::DateTime getAlarmTime();
+//     bool getAlarmState();
 
-};
+// };
 
-#endif
+// #endif
+
+#include "AlarmSystem.h"
+
+AlarmSystem::AlarmSystem() {
+  // TODO: stub
+}
+
+void AlarmSystem::update(InputEvent i) {
+  // TODO: stub
+}
---
+++ src/Full_program_v3.9/AlarmSystem.h
@@ -0,0 +1,29 @@
+#ifndef ALARMSYSTEM_H
+#define ALARMSYSTEM_H
+
+#include "Observer.h"
+#include <Ds1302.h>
+
+// Controls the functionality of the clock & alarm system 
+class AlarmSystem : public Observer {
+  private:
+    Ds1302::DateTime alarmTime;
+    short fadeTime = 20;
+    bool alarmIsOn = false;
+    unsigned long ledStartTime;
+    unsigned long waitTime;
+
+    void updateLight();
+    void daylightSavingsChange(short change); // Could make this bool since only two states- +1hr or -1hr
+    void setAlarmTime(Ds1302::DateTime t);
+    void setAlarmState(bool s);
+
+  public:
+    AlarmSystem();
+    void update(InputEvent i) override;
+    Ds1302::DateTime getAlarmTime();
+    bool getAlarmState();
+
+};
+
+#endif
