3411bcbc4cfb4c1126173d78a89163713f78b36f
Breanne Chan <107957421+nifty-knight@users.noreply.github.com>
2025-08-21 17:52:10 -0700

    Added LightController.h file

--- src/Full_program_v3.9/LightController.cpp
+++ src/Full_program_v3.9/LightController.cpp
@@ -1,25 +1,32 @@
-#ifndef LIGHTCONTROLLER_H
-#define LIGHTCONTROLLER_H
+// #ifndef LIGHTCONTROLLER_H
+// #define LIGHTCONTROLLER_H
 
-#include <Arduino.h>
+// #include <Arduino.h>
 
-// Responsible for all functionality related to modifying the brightness of the LED light
-class LightController {
-  private:
-  // TODO: initialize all vars that need it - may not store ledPin, but take it in via constructor
-    uint8_t ledPin;
-    uint8_t brightness;
+// // Responsible for all functionality related to modifying the brightness of the LED light
+// class LightController {
+//   private:
+//   // TODO: initialize all vars that need it - may not store ledPin, but take it in via constructor
+//     uint8_t ledPin;
+//     uint8_t brightness;
 
 
-  public:
-    uint8_t getBrightness(); // note: uint8_t is an unsigned int iwth 8 bits, storing values from 0-255
-    void setBrightness(uint8_t b);
-    bool lightIsOn();
-    // void turnLightOff();
-};
+//   public:
+//     uint8_t getBrightness(); // note: uint8_t is an unsigned int iwth 8 bits, storing values from 0-255
+//     void setBrightness(uint8_t b);
+//     bool lightIsOn();
+//     // void turnLightOff();
+// };
 
-#endif
+// #endif
 
-// uint8_t LightController::LightController getBrightness() {
-//   return 1;
-// }
+#include "LightController.h"
+
+LightController::LightController() {
+  // TODO: stub
+}
+
+uint8_t LightController::getBrightness() {
+  // TODO: stub
+  return 1;
+}
