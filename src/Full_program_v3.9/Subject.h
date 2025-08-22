dea8e1e12c8507bde34da42d823d826b9b8a5938
Breanne Chan <107957421+nifty-knight@users.noreply.github.com>
2025-08-21 18:38:29 -0700

    Changed to Subject.h file

--- src/Full_program_v3.9/Subject.h
+++ src/Full_program_v3.9/Subject.h
@@ -1,6 +1,7 @@
 #ifndef SUBJECT_H
 #define SUBJECT_H
 
+#include <Arduino.h>
 #include "Observer.h" // TODO: uncomment, replace .cpp
 // #include "Observer.cpp"
 
@@ -8,9 +9,14 @@
 // TODO: I just realized, if this is an interface maybe I should just make a Subject.h file, no .cpp equivalent
 class Subject {
   public:
+    // TODO: See if there's a better way to create the observer array than making it fixed-length
+    static const uint8_t numObservers = 2; // TODO: This limits the max number of observers, must change if need more observers 
     virtual void addObserver(Observer o) = 0;
     // virtual void dropObserver(Observer o) = 0;
     virtual void notifyObservers(InputEvent i) = 0;
+
+  protected:
+      Observer observers[numObservers];
 };
 
 #endif
