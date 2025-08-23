9c7af30568464ac4b2319f5d917aad208c0b2205
Breanne Chan <107957421+nifty-knight@users.noreply.github.com>
2025-08-23 14:18:22 -0700

    Added Clock.h file, added include path

--- src/Full_program_v3.9/Clock.cpp
+++ src/Full_program_v3.9/Clock.cpp
@@ -1,25 +1,4 @@
-#ifndef CLOCK_H
-#define CLOCK_H
-
-#include <Arduino.h>
-#include <Ds1302.h>
-
-// Responsible for all functions dealing with the time tracked by the RTC
-class Clock {
-  private:
-    // const uint8_t PIN_CLK = 7;
-    // const uint8_t PIN_DAT = 6;
-    // const uint8_t PIN_ENA = 5;  // RST on the module itself
-
-  public:
-    Clock(uint8_t clk, uint8_t dat, uint8_t ena);
-    Ds1302::DateTime getTime();
-    void setTime(Ds1302::DateTime t);
-};
-
-uint8_t convertTo24HrTime(uint8_t hr, String period); // TODO; might make it return like a 4-byte struct if it would save space - then again, that might be a large deep rabbit hole
-
-#endif
+#include "Clock.h"
 
 // EFFECTS: Initializes rtc module & pins
 //  TODO: add autofilled null/none parameter for time, modify constructor to sets rtc to given time if one is given
---
+++ src/Full_program_v3.9/Clock.h
@@ -0,0 +1,22 @@
+#ifndef CLOCK_H
+#define CLOCK_H
+
+#include <Arduino.h>
+#include <Ds1302.h>
+
+// Responsible for all functions dealing with the time tracked by the RTC
+class Clock {
+  private:
+    // const uint8_t PIN_CLK = 7;
+    // const uint8_t PIN_DAT = 6;
+    // const uint8_t PIN_ENA = 5;  // RST on the module itself
+
+  public:
+    Clock(uint8_t clk, uint8_t dat, uint8_t ena);
+    Ds1302::DateTime getTime();
+    void setTime(Ds1302::DateTime t);
+};
+
+uint8_t convertTo24HrTime(uint8_t hr, String period); // TODO; might make it return like a 4-byte struct if it would save space - then again, that might be a large deep rabbit hole
+
+#endif
