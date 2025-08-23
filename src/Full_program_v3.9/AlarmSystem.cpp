114eb80ad20509dc43fefe60c8006dfe9cc543ec
Breanne Chan <107957421+nifty-knight@users.noreply.github.com>
2025-08-23 14:33:03 -0700

    Added function docs, stubs, test cases

--- src/Full_program_v3.9/AlarmSystem.cpp
+++ src/Full_program_v3.9/AlarmSystem.cpp
@@ -29,10 +29,70 @@
 
 #include "AlarmSystem.h"
 
-AlarmSystem::AlarmSystem() {
+// EFFECTS: initializes necessary fields & subclass objects & set initial alarmTime to 1:00am
+// TODO: maybe include alarmTime as an optional parameter?
+AlarmSystem::AlarmSystem(uint8_t fadeTime, uint8_t clk, uint8_t dat, uint8_t ena, uint8_t ledPin) : clock(clk, dat, ena), lightController(ledPin) {
+  clock = Clock(clk, dat, ena);
   // TODO: stub
 }
 
+// EFFECTS: Updates the state of the light and clock as necessary
+/*Test cases
+- update(SelectButtonPressed) --> turn light off if it is on, nothing otherwise
+- Ignore all other events - do the same thing in any case
+- Check that light turns on at the correct time
+- Check that light does not turn on if alarmIsOn is off
+- Check that light gets brighter at the correct times - as time progresses
+*/
 void AlarmSystem::update(InputEvent i) {
   // TODO: stub
+}
+
+// // EFFECTS: updates the light according to the correct variables
+// // TODO: do I even need this function? Don't think so honestly
+// void AlarmSystem::updateLight() {
+//   // TODO: stub
+// }
+
+// MODIFIES: 
+// EFFECTS: 
+void AlarmSystem::daylightSavingsChange(bool plus) {
+
+}
+
+// MODIFIES: alarmTime, alarmIsOn
+// EFFECTS: Sets alarmTime to t and sets alarmIsOn to true
+/*Test cases
+- test setting it multiple times 
+- check that alarmIsOn ends up true regardless of whether it started as false or true
+*/
+void AlarmSystem::setAlarmTime(Ds1302::DateTime t) {
+  // TODO: stub
+}
+
+// MODIFIES: alarmIsOn
+// EFFECTS: sets alarmIsOn to s
+/*Test cases
+- alarmIsOn = false, setAlarmState(true) --> alarmIsOn = true
+- alarmIsOn = false, setAlarmState(false) --> alarmIsOn = false
+- alarmIsOn = true, setAlarmState(true) --> alarmIsOn = true
+- alarmIsOn = true, setAlarmState(false) --> alarmIsOn = false
+*/
+void AlarmSystem::setAlarmState(bool s) {
+  // TODO: stub
+}
+
+// EFFECTS: Returns alarmTime
+// Getter - no test cases
+Ds1302::DateTime AlarmSystem::getAlarmTime() {
+  // TODO: stub
+  Ds1302::DateTime t;
+  return t;
+}
+
+// EFFECTS: Returns alarmIsOn
+// Getter - no test cases
+bool AlarmSystem::getAlarmState() {
+  // TODO: stub
+  return false;
 }
--- src/Full_program_v3.9/AlarmSystem.h
+++ src/Full_program_v3.9/AlarmSystem.h
@@ -2,27 +2,32 @@
 #define ALARMSYSTEM_H
 
 #include "Observer.h"
+#include "Clock.h"
+#include "LightController.h"
 #include <Ds1302.h>
 
 // Controls the functionality of the clock & alarm system 
 class AlarmSystem : public Observer {
   private:
     Ds1302::DateTime alarmTime;
-    short fadeTime = 20;
+    uint8_t fadeTime; //  = 20; // TODO: do I want to set max to be 60 minutes?
     bool alarmIsOn = false;
     unsigned long ledStartTime;
     unsigned long waitTime;
+    Clock clock;
+    LightController lightController;
 
-    void updateLight();
-    void daylightSavingsChange(short change); // Could make this bool since only two states- +1hr or -1hr
-    void setAlarmTime(Ds1302::DateTime t);
-    void setAlarmState(bool s);
+    // void updateLight();
 
   public:
-    AlarmSystem();
+    AlarmSystem(uint8_t fadeTime, uint8_t clk, uint8_t dat, uint8_t ena, uint8_t ledPin);
     void update(InputEvent i) override;
     Ds1302::DateTime getAlarmTime();
     bool getAlarmState();
+    void setAlarmTime(Ds1302::DateTime t);
+    void setAlarmState(bool s);
+    void daylightSavingsChange(bool plus); // TODO: Could make this bool since only two states- +1hr or -1hr
+
 
 };
 
