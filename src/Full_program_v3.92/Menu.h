f4e488e70e823c7efd73ce8428a2b7f946f14db1
Breanne Chan <107957421+nifty-knight@users.noreply.github.com>
2025-08-25 18:50:26 -0700

    Added custom arrow characters

--- src/Full_program_v3.92/Menu.h
+++ src/Full_program_v3.92/Menu.h
@@ -3,6 +3,50 @@
 
 #include "MenuItem.h"
 
+byte UpArrow[8] = {
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00100,
+  0b01110,
+  0b11111,
+  0b00000,
+  0b00000
+};
+
+byte DownArrow[8] = {
+  0b00000,
+  0b00000,
+  0b11111,
+  0b01110,
+  0b00100,
+  0b00000,
+  0b00000,
+  0b00000
+};
+
+byte RightArrow[8] = {
+  0b00000,
+  0b01000,
+  0b01100,
+  0b01110,
+  0b01110,
+  0b01100,
+  0b01000,
+  0b00000
+};
+
+byte LeftArrow[8] = {
+  0b00000,
+  0b00010,
+  0b00110,
+  0b01110,
+  0b01110,
+  0b00110,
+  0b00010,
+  0b00000
+};
+
 class Menu : public MenuItem {
   private:
     MenuItem* pages[5]; // note that this is a "magic number" hard limit on the max number of pages in a menu
