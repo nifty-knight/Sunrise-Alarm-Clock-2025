35aa73053c409a5026881df259fb8cf92c21026e
Breanne Chan <107957421+nifty-knight@users.noreply.github.com>
2025-08-25 18:41:47 -0700

    Added function docs, test cases, stubs

---
+++ src/Full_program_v3.92/Menu.cpp
@@ -0,0 +1,31 @@
+#include "Menu.h"
+
+// REQUIRES: pages contains equal to or less than the max number of pages allowed in Menu.h
+// EFFECTS: Initializes menu, adds pages to menu
+Menu::Menu(String line1, String line2, MenuItem* pages) : MenuItem(line1, line2) {
+  
+  // TODO: stub
+}
+
+// EFFECTS: shows message for this menu on the LCD
+// Tested in hardware
+void Menu::display() {
+  // TODO: stub
+}
+
+// REQUIRES: index is valid and within range of pages
+// EFFECTS: Shows message for child menuItem at given index
+// Tested in hardware - ensure that the page at the correct index is being displayed
+void Menu::displayChild(int index) {
+  // TODO: stub
+}
+
+// REQUIRES: There is room in pages to add another one
+// EFFECTS: Adds the given child to pages
+/*Test cases - low priority
+- Check that the child is being added - would need a getter for this
+- define behaviour for if pages is full
+*/
+void Menu::addChild(MenuItem* child) {
+  // TODO: stub
+}
