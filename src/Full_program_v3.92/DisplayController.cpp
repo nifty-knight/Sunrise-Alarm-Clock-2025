888cc67996607a77f95ea67bea0b0b792ac9ba10
Breanne Chan <107957421+nifty-knight@users.noreply.github.com>
2025-08-28 17:07:49 -0700

    Added implementations, modified class to take alarmSystem as a field

--- src/Full_program_v3.92/DisplayController.cpp
+++ src/Full_program_v3.92/DisplayController.cpp
@@ -1,11 +1,22 @@
 #include "DisplayController.h"
+#include "Arrows.h"
 
 // EFFECTS: initializes needed variables + output pin for LED
-DisplayController::DisplayController(int readTime, int noActivityTime, uint8_t backlightPin) : readTime(readTime), noActivityTime(noActivityTime), backlightPin(backlightPin) {
-    // TODO: stub    
+DisplayController::DisplayController(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, int readTime, int noActivityTime, uint8_t backlightPin, AlarmSystem alarmSystem) : 
+        lcd(rs, enable, d4, d5, d6, d7), readTime(readTime), noActivityTime(noActivityTime), backlightPin(backlightPin), alarmSystem(alarmSystem) {
+  // TODO: stub    
+  index = 0;
+  pinMode(backlightPin, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.clear();
+  this->off();
+  lcd.createChar(0, LeftArrow);
+  lcd.createChar(1, RightArrow);
+  lcd.createChar(2, UpArrow);
+  lcd.createChar(3, DownArrow);
 }
 
-// MODIFIES: index, startTime, 
+// MODIFIES: index, startTime, displayOn
 // EFFECTS: Updates LCD display depending on user event
 /*Test cases - NOTE: may not fully use unit tests for this function
 - None: checks if noActivityTime amount of time has passed since startTime - if so, turn off LCD; else, do nothing
@@ -15,6 +26,7 @@ DisplayController::DisplayController(int readTime, int noActivityTime, uint8_t b
 */
 void DisplayController::update(InputEvent i) {
   // TODO: stub
+  alarmSystem.update(i);
 }
 
 // MODIFIES: displayOn
@@ -23,7 +35,10 @@ void DisplayController::update(InputEvent i) {
 - when it is called, display and backlight both turn on
 */
 void DisplayController::on() {
-  // TODO: stub
+  lcd.display();
+  setBacklight(true);
+  displayOn = true;
+  this->resetActivityTimer();
 }
 
 // MODIFIES: displayOn
@@ -32,7 +47,9 @@ void DisplayController::on() {
 - when it is called, display and backlight both turn off
 */
 void DisplayController::off() {
-  // TODO: stub
+  lcd.noDisplay();
+  setBacklight(false);
+  displayOn = false;
 }
 
 // EFFECTS: sets the backlight on or off based on bool given (true = on, false = off)
@@ -41,13 +58,35 @@ void DisplayController::off() {
 - setBacklight(false) --> backlight off
 */
 void DisplayController::setBacklight(bool s) {
-  // TODO: stub
+  digitalWrite(backlightPin, s);
 }
 
+//
 // EFFECTS: prints an arrow on the lcd display at the current cursor location
+//          if the direction given is NONE, this function does nothing
 /*Test cases - must be hardware tested
 - Check that each direction arrow prints out correctly
 */
 void DisplayController::printArrow(Direction d) {
-  // TODO: stub
+  switch(d) {
+    case Direction::DOWN:
+      lcd.write(byte(3));
+      break;
+    case Direction::LEFT:
+      lcd.write(byte(0));
+      break;
+    case Direction::RIGHT:
+      lcd.write(byte(1));
+      break;
+    case Direction::UP:
+      lcd.write(byte(2));
+      break;
+    default: // do nothing
+      break;
+  }
+}
+
+// EFFECTS: Sets startTime to the current amount returned by millis()
+void DisplayController::resetActivityTimer() {
+  startTime = millis();
 }
--- src/Full_program_v3.92/DisplayController.h
+++ src/Full_program_v3.92/DisplayController.h
@@ -2,6 +2,7 @@
 #define DISPLAYCONTROLLER_H
 
 #include "InputEvent.h"
+#include "AlarmSystem.h"
 #include <LiquidCrystal.h>
 
 // Represents arrow directions; TODO: may put in separate file if multiple classes need access
@@ -22,24 +23,23 @@ class DisplayController {
     const int readTime;
     const int noActivityTime;
     const uint8_t backlightPin;
-    // int readTime;
-    // int noActivityTime;
-    // uint8_t backlightPin;
-    // LiquidCrystal lcd(13, 11, 10, 8, 3, 2); // TODO: You cannot instantiate an object in a class definition; if you really need this var, use a member initialization list
+    LiquidCrystal lcd;
     uint8_t index;
     unsigned long startTime;
     bool displayOn;
+
+    AlarmSystem alarmSystem;
 
     void on();
     void off();
     void setBacklight(bool s);
     void printArrow(Direction d); // TODO: note that it requires direction != NONE
-
+    void resetActivityTimer();
     // void select(); // Might use in implementation - don't have it all figured out yet
     // void escape();
 
   public:
-    DisplayController(int readTime, int noActivityTime, uint8_t backlightPin);
+    DisplayController(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, int readTime, int noActivityTime, uint8_t backlightPin, AlarmSystem alarmSystem);
     void update(InputEvent i);
 };
 
