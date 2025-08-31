67365f58f4284d199a6f89ead9877b89ddc01717
Breanne Chan <107957421+nifty-knight@users.noreply.github.com>
2025-08-31 08:15:50 -0700

    Create Direction.h

---
+++ src/Full_program_v3.92/Direction.h
@@ -0,0 +1,13 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+// Represents possible directions for arrows and joystick
+enum class Direction {
+  LEFT,
+  RIGHT,
+  UP,
+  DOWN,
+  NONE
+};
+
+#endif
