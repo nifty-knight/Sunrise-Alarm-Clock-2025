cde40bf83bb52a472a8b8172b9977486c9398d7a
Breanne Chan <107957421+nifty-knight@users.noreply.github.com>
2025-09-02 11:03:36 -0700

    Added framework for menu navigation

--- src/Full_program_v3.92/DisplayController.cpp
+++ src/Full_program_v3.92/DisplayController.cpp
@@ -4,8 +4,8 @@
 // EFFECTS: initializes needed variables + output pin for LED
 DisplayController::DisplayController(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, int readTime, int noActivityTime, uint8_t backlightPin) : 
         lcd(rs, enable, d4, d5, d6, d7), readTime(readTime), noActivityTime(noActivityTime), backlightPin(backlightPin) {
-  // TODO: stub    
   index = 0;
+  historySize = 0;
   pinMode(backlightPin, OUTPUT);
   lcd.begin(16, 2);
   lcd.clear();
@@ -16,6 +16,9 @@ DisplayController::DisplayController(uint8_t rs, uint8_t enable, uint8_t d4, uin
   lcd.createChar(3, DownArrow);
 }
 
+// TODO: lots of repetition of resetActivityTimer()
+// TODO: lots of repetition of if (displayOn); could be refactored to reduce repetition
+// TODO: lots of repetition & coupling between InputEvent and Direction; can I combine them somehow?
 // MODIFIES: index, startTime, displayOn
 // EFFECTS: Updates LCD display depending on user event
 /*Test cases - NOTE: may not fully use unit tests for this function
@@ -25,8 +28,55 @@ DisplayController::DisplayController(uint8_t rs, uint8_t enable, uint8_t d4, uin
 - 
 */
 void DisplayController::update(InputEvent i) {
-  // TODO: stub
-  // AlarmSystem::getInstance().update(i);
+  switch(i) {
+    case InputEvent::SelectButtonPressed:
+      if (!displayOn) {
+        on();
+        displayMainMenu();
+      } else {
+        // if page has children: show child
+        // else: reset menu hierarchy
+        // TODO: 
+        //       - for pages, select() resets the menu hierarchy
+        //       - for menus, select() calls displayChild() and also addPage() with the displayed child
+        getCurrentPage()->select();
+      }
+      break;
+    case InputEvent::TimeButtonPressed:
+      displayTimePage();
+      resetMenuHierarchy();
+      resetActivityTimer();
+      break;
+    case InputEvent::JoystickDown:
+      if (displayOn) {
+        getCurrentPage()->joystickResponse(Direction::DOWN);
+        resetActivityTimer();
+      }
+      break;
+    case InputEvent::JoystickUp:
+      if (displayOn) {
+        getCurrentPage()->joystickResponse(Direction::UP);
+        resetActivityTimer();
+      }
+      break;
+    case InputEvent::JoystickLeft:
+      if (displayOn) {
+        getCurrentPage()->joystickResponse(Direction::LEFT);
+        resetActivityTimer();
+      }
+      break;
+    case InputEvent::JoystickRight:
+      if (displayOn) {
+        getCurrentPage()->joystickResponse(Direction::RIGHT);
+        resetActivityTimer();
+      }
+      break;
+    default: // None
+      if (displayOn && ((millis() - startTime) > noActivityTime)) {
+        off();
+      }
+      break;
+  }
 }
 
 // MODIFIES: displayOn
@@ -61,7 +111,6 @@ void DisplayController::setBacklight(bool s) {
   digitalWrite(backlightPin, s);
 }
 
-//
 // EFFECTS: prints an arrow on the lcd display at the current cursor location
 //          if the direction given is NONE, this function does nothing
 /*Test cases - must be hardware tested
@@ -89,4 +138,46 @@ void DisplayController::printArrow(Direction d) {
 // EFFECTS: Sets startTime to the current amount returned by millis()
 void DisplayController::resetActivityTimer() {
   startTime = millis();
+}
+
+// REQUIRES: history is not empty; the screen must be on and displaying when this is called
+// EFFECTS: Returns the current page displayed to the user 
+MenuItem* DisplayController::getCurrentPage() {
+  // TODO: stub
+  Page item("", "");
+  return &item;
+}
+
+// MODIFIES: index, history, historySize // TODO: might not even need to modify history - historySize ensures it will get rewritten over
+// EFFECTS: resets index & clears history, displays first page of main menu
+void DisplayController::resetMenuHierarchy() {
+  // TODO: stub
+}
+
+// REQUIRES: history is not full; has room to store another page
+// MODIFIES: history, historySize
+// EFFECTS: Appends a page to the end of history array and updates historySize accordingly
+//          Note that this function does not display the given page; it is called by the selected page
+void DisplayController::addPage(MenuItem* page) {
+  // TODO: stub
+}
+
+// REQUIRES: history has at least 2 pages in it; cannot go back to an empty page
+// MODIFIES: history, historySize
+// EFFECTS: Removes the current page from history, updates historySize accordingly, displays the previous page
+//          or the first page of the previous menu
+void DisplayController::back() {
+  // TODO: stub
+}
+
+// MODIFIES: index, history, historySize // TODO: maybe not history if I use circular buffer-style storage
+// EFFECTS: Displays the page showing current time to the user
+void DisplayController::displayTimePage() {
+  // TODO: stub
+}
+
+// MODIFIES: index, history, historySize
+// EFFECTS: Displays first page of the main menu
+void DisplayController::displayMainMenu() {
+  // TODO: stub
 }
--- src/Full_program_v3.92/DisplayController.h
+++ src/Full_program_v3.92/DisplayController.h
@@ -4,6 +4,8 @@
 #include "InputEvent.h"
 #include "AlarmSystem.h"
 #include "Direction.h"
+#include "Menu.h"
+#include "Page.h"
 #include <LiquidCrystal.h>
 
 // Responsible for displaying menus and visuals to the user via the LCD
@@ -14,23 +16,37 @@ class DisplayController {
     const int noActivityTime;
     const uint8_t backlightPin;
     LiquidCrystal lcd;
-    uint8_t index;
     unsigned long startTime;
     bool displayOn;
 
-    // AlarmSystem alarmSystem;
+    uint8_t index; // for keeping track of pages with the same parent menu
+    MenuItem* history[10]; // Contains page traversal history; Treat this like a stack
+    // TODO: hard "magic number" limit of 10 pages in history - can I change this to be better?
+    // TODO: create a stack class to use instead of array
+    uint8_t historySize; // Keeps track of current number of pages in history
 
     void on();
     void off();
     void setBacklight(bool s);
-    void printArrow(Direction d); // TODO: note that it requires direction != NONE
+    void printArrow(Direction d); 
     void resetActivityTimer();
+    MenuItem* getCurrentPage(); 
+    void displayTimePage(); 
+    void displayMainMenu(); 
+    void increaseIndex() { index++; };
+    void decreaseIndex() { index--; };
+
     // void select(); // Might use in implementation - don't have it all figured out yet
-    // void escape();
 
   public:
     DisplayController(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, int readTime, int noActivityTime, uint8_t backlightPin);
     void update(InputEvent i);
+    void addPage(MenuItem* page); 
+    void back(); 
+    uint8_t getIndex() { return index; };
+    void resetIndex() { index = 0; };
+    void resetMenuHierarchy();
+
 };
 
 #endif
